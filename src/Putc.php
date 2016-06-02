0eec019bbb1f3a75e11ce55b0921b4c9401746cb
Александр Багышов <bagishov@1mps.ru>
2016-06-02 10:54:19 +0300

    burned

---
+++ src/Putc.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Putc means PHPUnit TestCase.
+ * It's helper for testing prootected fields and methods.
+ */
+class Putc extends PHPUnit_Framework_TestCase {
+
+	protected function invokeMethod($object, $methodName, array $parameters = array()) {
+		$reflection = new \ReflectionClass(get_class($object));
+		$method = $reflection->getMethod($methodName);
+		$method->setAccessible(true);
+
+		return $method->invokeArgs($object, $parameters);
+	}
+
+	protected function invokeProperty($object, $propertyName) {
+		$reflector = new ReflectionClass($object);
+		$property = $reflector->getProperty($propertyName);
+		$property->setAccessible(true);
+
+		return $property->getValue($object);
+	}
+
+}
